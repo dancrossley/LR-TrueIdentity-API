79c6438323b6e6bc8189024d59a1c512db745640
Dan Crossley <dan.crossley@gmail.com>
2021-05-26 15:00:25 +1000

    Update add_id.py

--- add_id.py
+++ add_id.py
@@ -5,13 +5,13 @@ requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning) # Ig
 
 ###########################
 # Change these 
-pm = 'UKRD1SEXM-01' # URL / IP of the PM
+pm = '127.0.0.1:8501' # URL / IP of the PM along with the port if its on-prem. For example 'x.x.x.x:8501'. Omit the ':8501' for LR Cloud.
 filename = "C:\\Users\dcrossley\\Documents\\test.txt" # Put the fullpath to the file with the usernames here, double escape the folders, ie '\\'
 email_suffix = '@logrhythm.com' # This is used for the unique identifier for the Identity
-api_token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjE1LCJqdGkiOiI3NTY3ZGUwMi1iNWEzLTQ4NzItYjVlMi1lZDAyNjg4Y2U3OTUiLCJjaWQiOiI1RkEyOUEyNi1FREJBLTQ4RjQtQTM5RC02M0I3RTFDQ0I1NjkiLCJpc3MiOiJsci1hdXRoIiwicmlkIjoiZ2xvYmFsQWRtaW4iLCJwaWQiOjE1LCJzdWIiOiJVS0NYQ1xcZGNyb3NzbGV5IiwiZXhwIjoxNjUzMzY0NjE2LCJkZWlkIjoxLCJpYXQiOjE2MjE4Mjg2MTZ9.JcjoC_ZBuUN7qB5QDdblDAtM6Zn56jbhWcScMJkqO4IqJgCBN_QclzxtZyBMuKPzib-gz9dcyImxDytIPXml5GijNCigEnl19KTbDaEgxvDD3nsf2CIYj0zAeJ_h48pCAJjzUJj4zTzoOL9HGfnOMVyZZ5hB7WatzJu16pmFCgZETduHCmbQHG3CTNBHAgIKXetxyf4Cfp9-DRUD-TBexQdXeGkHrjzUMmKTXdshmzT-ooxn-_WhEGcRyjzWYLHeVZXnBQlKL_RSgPapJtvHXQGAKQ6KcBCRFxnyAEtzH7lYkhl5CFVcKxOWXAOhzM0CJIykAmm381TesorWRwwCNQ'
+api_token = ''
 ###########################
 
-url = ("https://%s:8501/lr-admin-api/identities/bulk/?entityID=1" % pm)
+url = ("https://%s/lr-admin-api/identities/bulk/?entityID=1" % pm)
 
 def process_names(filename):
     # reads firstname.lastname from a textfile and posts it to the API as an Identity
@@ -29,7 +29,7 @@ def process_names(filename):
         call_api(identity)
     
 def call_api(identity):
-    # Posts identity to the LogRhythm AP
+    # Posts identity to the LogRhythm API
     t = 'Bearer ' + api_token
     headers = {
     'Authorization': t,
